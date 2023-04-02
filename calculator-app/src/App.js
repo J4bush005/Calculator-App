6294731dc53d4bdb644394037071d701a97700cd
Johnathon Forbush <jtforbush429@gmail.com>
2023-04-01 22:38:32 -0500

    Added Buttons

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -2,6 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return(
+    <>
+      <div className="calc">
+        <input type="text" placeholder="0" id="answer" />
+        <input type="button" value="9" className="button"/>
+        <input type="button" value="8" className="button"/>
+        <input type="button" value="7" className="button"/>
+        <input type="button" value="6" className="button"/>
+        <input type="button" value="5" className="button"/>
+        <input type="button" value="4" className="button"/>
+        <input type="button" value="3" className="button"/>
+        <input type="button" value="2" className="button"/>
+        <input type="button" value="1" className="button"/>
+
+        <input type="button" value="0" className="button"/>
+        <input type="button" value="+" className="button"/>
+        <input type="button" value="-" className="button"/>
+        <input type="button" value="/" className="button"/>
+        <input type="button" value="*" className="button"/>
+        <input type="button" value="%" className="button"/>
+      </div>
+    </>
+  )
 
 }
 
